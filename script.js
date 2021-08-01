8514df5e5055dae95d055ba48ea691a14a9fa957
Suzana <s.dasilvasuzuki@gmail.com>
2021-08-01 15:16:42 -0300

    alteraçõestwo

--- script.js
+++ script.js
@@ -29,4 +29,33 @@ function updateMargin() {
 
 }
 
+const imageElements = document.querySelectorAll('.noanimation');
+
+window.onscroll = function() {
+    for (let element of imageElements) {
+        checkVisible(element) ? addAnimation(element) : removeAnimation(element);
+    }
+    
+};
+  
+  function checkVisible(elementTarget) {
+    let rect = elementTarget.getBoundingClientRect();
+    let elemTop = rect.top;
+    let elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    //let isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+  }
+
+  function addAnimation(element) {
+      element.classList.add('animation');
+  }
+
+  function removeAnimation(element) {
+      element.classList.remove('animation');
+  }
+
 //setInterval(goNext, 6000);
